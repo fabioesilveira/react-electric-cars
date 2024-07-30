431e2d57a96d78276f7117108afdda1f94ca9b59
Alberto <betim009@gmail.com>
2024-07-30 11:42:03 -0300

    seed

--- backEnd/src/db/seed.sql
+++ backEnd/src/db/seed.sql
@@ -89,3 +89,221 @@ VALUES
         'https://www.youtube.com/embed/K4Pv2tohydE?si=JHbpzScx_BPVkSIR',
         FALSE
     );
+
+INSERT INTO
+    users (name, email, password)
+VALUES
+    ('Fabio', 'fabio@email.com', '123');
+
+INSERT INTO
+    cars (
+        car_name,
+        model,
+        price,
+        `range`,
+        image_1,
+        image_2,
+        image_3,
+        image_4,
+        video,
+        is_show
+    )
+VALUES
+    (
+        'MODEL 3-2024',
+        'Tesla',
+        49130.00,
+        '341 miles',
+        'https://hips.hearstapps.com/hmg-prod/images/2024-tesla-model-3-blue-659ebaabd4e71.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2024-tesla-model-3-front-three-quarters-in-motion-659ebaaaaa10f.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2024-tesla-model-3-front-interior-659ebaa9e070d.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2024-tesla-model-3-rear-seat-659ebab040bc4.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://www.youtube.com/embed/K4Pv2tohydE?si=JHbpzScx_BPVkSIR',
+        FALSE
+    ),
+    (
+        'MODEL S-2024',
+        'Tesla',
+        74630.00,
+        '405 miles',
+        'https://hips.hearstapps.com/hmg-prod/images/2021-tesla-model-s-plaid-107-1638811396.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2021-tesla-model-s-plaid-108-1638811396.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2021-tesla-model-s-plaid-102-1638811395.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2021-tesla-model-s-plaid-112-1638811399.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://www.youtube.com/embed/K4Pv2tohydE?si=JHbpzScx_BPVkSIR',
+        FALSE
+    ),
+    (
+        'MODEL Y-2024',
+        'Tesla',
+        54130.00,
+        '310 miles',
+        'https://hips.hearstapps.com/hmg-prod/images/2020-tesla-model-y-long-range-101-1592842279.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2020-tesla-model-y-long-range-102-1592842278.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2020-tesla-model-y-long-range-103-1592842278.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2020-tesla-model-y-long-range-104-1592842278.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://www.youtube.com/embed/K4Pv2tohydE?si=JHbpzScx_BPVkSIR',
+        FALSE
+    ),
+    (
+        'MODEL X-2024',
+        'Tesla',
+        79630.00,
+        '335 miles',
+        'https://hips.hearstapps.com/hmg-prod/images/2020-tesla-model-x-101-656e382168a37.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2020-tesla-model-x-109-656e3821a0e49.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2020-tesla-model-x-117-656e3822b0ec0.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2020-tesla-model-x-114-656e37ef76212.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://www.youtube.com/embed/K4Pv2tohydE?si=JHbpzScx_BPVkSIR',
+        FALSE
+    ),
+    (
+        'CYBERTRUCK 2024',
+        'Tesla',
+        101985.00,
+        '340 miles',
+        'https://hips.hearstapps.com/hmg-prod/images/2024-tesla-cybertruck-102-65e894591b71d.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2024-tesla-cybertruck-beast-1105-6632475b014db.jpeg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2024-tesla-cybertruck-107-65e8945c693e2.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2024-tesla-cybertruck-115-662a6d20c035b.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://www.youtube.com/embed/K4Pv2tohydE?si=JHbpzScx_BPVkSIR',
+        FALSE
+    ),
+    (
+        'ROADSTER 2026',
+        'Tesla',
+        200000.00,
+        '620 miles',
+        'https://hips.hearstapps.com/hmg-prod/amv-prod-cad-assets/wp-content/uploads/2017/11/Tesla-Roadster-108.jpg?resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/amv-prod-cad-assets/wp-content/uploads/2017/11/Tesla-Roadster-105.jpg?resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/amv-prod-cad-assets/wp-content/uploads/2017/11/Tesla-Roadster-104.jpg?resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/amv-prod-cad-assets/wp-content/uploads/2017/11/Tesla-Roadster-111.jpg?resize=980:*',
+        'https://www.youtube.com/embed/K4Pv2tohydE?si=JHbpzScx_BPVkSIR',
+        FALSE
+    );
+
+INSERT INTO
+    cars (
+        car_name,
+        model,
+        price,
+        `range`,
+        image_1,
+        image_2,
+        image_3,
+        image_4,
+        video,
+        is_show
+    )
+VALUES
+    (
+        'R1S 2025',
+        'Rivian',
+        77700.00,
+        '270 miles',
+        'https://hips.hearstapps.com/hmg-prod/images/2025-rivian-r1s-106-665f7d86f3f50.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2025-rivian-r1s-115-665f7d8d51e8f.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2025-rivian-r1s-129-665f7d932d3d9.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2025-rivian-r1s-113-665f7d8d81d10.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        NULL,
+        FALSE
+    ),
+    (
+        'R1T 2025',
+        'Rivian',
+        71700.00,
+        '258 miles',
+        'https://hips.hearstapps.com/hmg-prod/images/2025-rivian-r1t-105-665f78ba81fdb.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2025-rivian-r1t-121-665f78c288f6a.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2025-rivian-r1t-102-665f78bcf3154.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2025-rivian-r1t-101-665f78bc37347.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        NULL,
+        FALSE
+    ),
+    (
+        'R2 2026',
+        'Rivian',
+        45000.00,
+        '300 miles',
+        'https://hips.hearstapps.com/hmg-prod/images/12-rivian-r2-65ea135495548.jpg?crop=1.00xw:0.847xh;0,0.153xh&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/11-rivian-r2-65ea135c4ead7.jpg?crop=1xw:0.843328335832084xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/05-accessories-65ea13646b0de.jpg?crop=1.00xw:0.847xh;0,0.122xh&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/rivian-r2-interior-hero-copy-65ea13ca5d23f.jpg?crop=1.00xw:0.968xh;0,0.0321xh&resize=980:*',
+        NULL,
+        FALSE
+    ),
+    (
+        'R3 2027',
+        'Rivian',
+        37000.00,
+        '300 miles',
+        'https://hips.hearstapps.com/hmg-prod/images/10-rivian-r3-65ea148745a44.jpg?crop=1xw:0.843328335832084xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/11-rivian-r3-65ea14813906b.jpg?crop=1.00xw:0.847xh;0,0.0306xh&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/r3-interior-65ea146f52861.jpg?crop=1xw:0.843328335832084xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/01-rivian-r3x-65ea14ae2d8cc.jpg?crop=1.00xw:0.847xh;0,0.105xh&resize=980:*',
+        NULL,
+        FALSE
+    );
+
+INSERT INTO
+    cars (
+        car_name,
+        model,
+        price,
+        `range`,
+        image_1,
+        image_2,
+        image_3,
+        image_4,
+        video,
+        is_show
+    )
+VALUES
+    (
+        'GENESIS GV60',
+        'Genesis',
+        61900.00,
+        '235 miles',
+        'https://hips.hearstapps.com/hmg-prod/images/2023-genesis-gv60-181-1657247820.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2023-genesis-gv60-142-1657247820.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2023-genesis-gv60-105-1657247820.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2023-genesis-gv60-976-1657247834.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        NULL,
+        FALSE
+    ),
+    (
+        'FORD MUSTANG MATCH-E',
+        'Ford',
+        55890.00,
+        '250 miles',
+        'https://hips.hearstapps.com/hmg-prod/images/2024-ford-mustang-mach-e-rally-2-64f8805a9ed2d.jpg?crop=0.718xw:0.605xh;0.136xw,0.306xh&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2024-ford-mustang-mach-e-rally-5-64f8805ba6e5a.jpg?crop=0.770xw:0.651xh;0.100xw,0.255xh&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2024-ford-mustang-mach-e-rally-7-64f880598802d.jpg?crop=0.687xw:0.579xh;0.128xw,0.311xh&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2024-ford-mustang-mach-e-rally-23-64f880608785c.jpg?crop=1.00xw:0.847xh;0,0.0689xh&resize=980:*',
+        NULL,
+        FALSE
+    ),
+    (
+        'PORSCHE TAYCAN',
+        'Porsche',
+        120495.00,
+        '246 miles',
+        'https://hips.hearstapps.com/hmg-prod/images/2025-porsche-taycan-103-660b0446c57f6.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2025-porsche-taycan-102-660b044633b0c.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2025-porsche-taycan-108-660b04426b9b4.jpg?crop=1.00xw:0.446xh;0,0.280xh&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2025-porsche-taycan-114-660b044858aaf.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        NULL,
+        FALSE
+    ),
+    (
+        'AUDI Q8 E-TRON',
+        'Audi',
+        78995.00,
+        '300 miles',
+        'https://hips.hearstapps.com/hmg-prod/images/2024-audi-q8-sportback-s-line-e-tron-prestige-402-64b97355b2365.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2024-audi-q8-sportback-s-line-e-tron-prestige-476-64b97378af76f.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2024-audi-q8-sportback-s-line-e-tron-prestige-420-64b97379cc68c.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        'https://hips.hearstapps.com/hmg-prod/images/2024-audi-q8-sportback-s-line-e-tron-prestige-453-64b9735710cd8.jpg?crop=1xw:1xh;center,top&resize=980:*',
+        NULL,
+        FALSE
+    );
