a7a81e433e93f8e39d80b8f6d25dd2e3093edef1
Fabio Silveira <fabio.eds@hotmail.com>
2024-08-04 22:38:54 -0700

    updatedCode

--- frontEnd/src/pages/LuxuryCars.jsx
+++ frontEnd/src/pages/LuxuryCars.jsx
@@ -3,18 +3,36 @@ import Footer from "../components/Footer";
 import NavBar from "../components/Navbar";
 import { luxuryCars } from "../data/data";
 import ListCars from "../components/ListCars";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 
 
 function LuxuryCars() {
+    const [cars, setCars] = useState(null)
+
+    useEffect(() => {
+        async function fetchAPI() {
+            const url = "http://localhost:3000/cars"
+            const response = await axios.get(url)
+            const data = response.data
+            const newData = data.filter((element) => element.model === "Luxury Cars")
+            setCars(newData)
+        }
+        fetchAPI()
+
+    }, [])
+
     return (
         <>
             <NavBar />
 
             <Container>
 
-                <ListCars cars={luxuryCars} />
-                
+                {cars ? <ListCars data={cars} /> : <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>}
+
             </Container>
 
             <Footer />
